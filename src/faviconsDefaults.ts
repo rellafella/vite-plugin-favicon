1c1f133508a5b757570a36e248b70890bcfe29f7
Liam Rella <liam.rella@gmail.com>
2025-03-24 09:13:28 +1030

    chore: update favicons defaults for favicons 7

--- src/faviconsDefaults.ts
+++ src/faviconsDefaults.ts
@@ -4,7 +4,7 @@ import { ViteFaviconsPluginOptions } from './index.js';
 export const getDefaultFaviconConfig = (options: ViteFaviconsPluginOptions): Partial<FaviconOptions> => {
 	const fOptions = options.favicons;
 	return {
-		appShortName: null,                       // Your application's short_name. `string`. Optional. If not set, appName will be used
+		appShortName: undefined,                       // Your application's short_name. `string`. Optional. If not set, appName will be used
 		dir: 'auto',                              // Primary text direction for name, short_name, and description
 		lang: 'en-US',                            // Primary language for name and short_name
 		background: '#fff',                       // Background colour for flattened icons. `string`
@@ -15,7 +15,6 @@ export const getDefaultFaviconConfig = (options: ViteFaviconsPluginOptions): Par
 		scope: '/',                               // set of URLs that the browser considers within your app
 		start_url: '/?homescreen=1',              // Start URL when launching the application from a device. `string`
 		version: '1.0',                           // Your application's version string. `string`
-		logging: false,                           // Print logs to console? `boolean`
 		pixel_art: false,                         // Keeps pixels "sharp" when scaling up, for pixel art.  Only supported in offline mode.
 		loadManifestWithCredentials: false,       // Browsers don't send cookies when fetching a manifest, enable this to fix that. `boolean`
 		...fOptions,
@@ -33,9 +32,7 @@ export const getDefaultFaviconConfig = (options: ViteFaviconsPluginOptions): Par
 			android: true,              // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
 			appleIcon: true,            // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
 			appleStartup: true,         // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-			coast: true,                // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
 			favicons: true,             // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
-			firefox: true,              // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
 			windows: true,              // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
 			yandex: true,                // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
 			...fOptions?.icons,
